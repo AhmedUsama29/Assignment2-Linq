beba8d6a715821656cbdfdccd714eb3aff50c07e
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-02-18 12:59:46 +0200

    First 2 Questions

--- Assignment2 Linq/Program.cs
+++ Assignment2 Linq/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Assignment2_Linq
+﻿using static Assignment2_Linq.ListGenerators;
+
+namespace Assignment2_Linq
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Element Operators
+
+
+            #region Q1 : Get first Product out of Stock
+
+            //var res = ProductList.First();
+
+            //Console.WriteLine(res);
+
+            #endregion
+
+            #region Q2 : Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+
+            //var res = ProductList.FirstOrDefault(p => p.UnitPrice > 1000);
+
+            //Console.WriteLine(res);
+
+            #endregion
+
+            #region Q3 : 
+
+            #endregion
+
+            #endregion
         }
     }
 }
